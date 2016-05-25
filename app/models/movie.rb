68e4f8d323c95877d01ca628b26642533d8e73b3
TanveetGill <tanveet.gill@gmail.com>
2016-05-24 21:19:01 -0400

    some extra features

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,19 @@
 class Movie < ActiveRecord::Base
+
+	validates_presence_of :title
+	validates_presence_of :director
+	validates_presence_of :description
+	validates_presence_of :release_date
+	validates_presence_of :poster_image_url
+	validates :runtime_in_minutes, numericality: { only_integer: true }
+	validate :release_date_is_in_the_past
+
+	protected
+
+	def release_date_is_in_the_past
+		if release_date.present?
+			errors.add(:release_date, "should be in the past") if release_date > Date.today
+		end
+	end
+	
 end
